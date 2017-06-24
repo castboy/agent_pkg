f747ab1e2cbdce1eda36649a3fa645628a12485b
castboy <wangmingguang@byzoro.com>
2017-06-24 18:10:50 +0800

    to do in home

---
+++ hdfs.go
@@ -0,0 +1,102 @@
+package agent_pkg
+
+import (
+    "github.com/colinmarc/hdfs"
+    "fmt"
+    "io/ioutil"
+    "crypto/sha256" 
+)
+
+const (
+    PRTNNUM = 128
+)
+
+type ToUpdateDataInfo struct {
+    Topic string
+    DataIndex int
+} 
+
+type HdfsToLocalReqParams struct {
+    File string
+    Offset int64
+    Size int
+
+    XdrMark string
+    
+    ToUpdateData ToUpdateDataInfo 
+}
+
+var HdfsToLocalReqChs [PRTNNUM]chan HdfsToLocalReqParams
+ 
+var client *hdfs.Client
+
+func InitHdfsCli (namenode string) {
+    client, _ = hdfs.New(namenode)
+}
+
+func HdfsToLocal (idx int) {
+    p := <-HdfsToLocalReqChs[idx]
+
+    bytes, runTime := hdfsRd(p)
+    ok := isRightFile(bytes, p.XdrMark)
+    if ok {
+        file := p.File + "_" + strconv.Itoa(p.Offset) + "_" + 
+                strconv.Itoa(p.Size) + "_" + strconv.Itoa(runTime) 
+        localWrite(file, bytes)
+    }
+    
+}
+
+func hdfsRd (ch chan HdfsToLocalReqParams) (bytes []byte, runTime int) {
+    beginTime := time.Now().Nanosecond()
+
+    if fileIsExist(ch.File) {
+        file, _ := client.Open(ch.File)
+        bytes := make([]byte, ch.Size)
+        file.ReadAt(bytes, ch.Offset)
+    }
+
+    endTime := time.Now().Nanosecond()
+    
+    return buf, endTime - beginTime 
+}
+
+func fileIsExist(file string) (bool) {
+    var exist = true;
+    if _, err := os.Stat(file); os.IsNotExist(err) {
+        exist = false;
+    }
+    return exist;
+}
+
+func isRightFile (hdfs []byte, xdrMark string) {
+    right := true
+ 
+    if xdrMark != getSha256Code(hdfs) {
+        right = false
+    }
+ 
+    return right     
+}
+
+func sha256Code (bytes []byte) string {
+    h := sha256.New()
+    h.Write(bytes)
+    return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func localWrite (file string, bytes []byte) {
+    err := ioutil.WriteFile(file, bytes, 0666)
+    if nil != err {
+        fmt.Println("LocalWrite Err")
+    }
+}
+
+func updateXdr (info ToUpdateDataInfo, localFile string) {
+    bytes := CacheDataMap[info.Topic][info.DataIndex]
+    
+    insertStr = ", \"File\": " + localFile + "}" 
+
+    bytes = bytes[]
+}
+
