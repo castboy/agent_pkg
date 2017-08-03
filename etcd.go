0ec85498d7595472261d604c59e4dab08a93b26e
yuexiriguang <482819895@qq.com>
2017-08-03 16:07:01 +0800

    debug

--- etcd.go
+++ etcd.go
@@ -50,7 +50,6 @@ func Record(seconds int) {
 		s.Status[0] = status["waf"]
 		s.Status[1] = status["vds"]
 		s.Status[2] = status["rule"]
-		fmt.Println("status[vds]", status["vds"])
 
 		byte, _ := json.Marshal(s)
 		EtcdSet("apt/agent/status/"+Localhost, string(byte))
