3d8965e354fd1b88946e04d24fac17a4dc2657ef
yuexiriguang <482819895@qq.com>
2017-08-05 18:56:10 +0800

    debug

--- waf.go
+++ waf.go
@@ -88,7 +88,10 @@ func ReqRule(instance, topic, srvIp string, srvPort int) {
 		Rule string
 	}
 
-	json.Unmarshal(body, &ruleRes)
+	err := json.Unmarshal(body, &ruleRes)
+	if nil != err {
+		fmt.Println("json.Unmarshal err")
+	}
 
 	fmt.Println("ruleRes.Rule", ruleRes.Rule)
 
