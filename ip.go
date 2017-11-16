1615fab4d69537c561f004c4a267a6623e765c0f
yuexiriguang <482819895@qq.com>
2017-11-16 17:52:56 +0800

    add Getenv(THIS_HOST) to be Localhost

--- ip.go
+++ ip.go
@@ -3,46 +3,50 @@
 package agent_pkg
 
 import (
-    "os"
-    "net"
-    "fmt"
-    "regexp"
+	"fmt"
+	"net"
+	"os"
+	"regexp"
 )
 
 var Localhost string
 var Partition int32
 
 func GetLocalhost() {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        os.Stderr.WriteString("Oops:" + err.Error())
-        os.Exit(1)
-    }
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops:" + err.Error())
+		os.Exit(1)
+	}
 
-    var ips []string
-    for _, a := range addrs {
-        if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-            if ipnet.IP.To4() != nil {
-                ips = append(ips, ipnet.IP.String())
-            }
-        }
-    }
-    for _, ip := range ips {
-        match, _ := regexp.MatchString("^192.*", ip)
-        if match {
-            Localhost = ip
-        }
-    }
-    fmt.Println("Localhost   : ", Localhost)
+	var ips []string
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips = append(ips, ipnet.IP.String())
+			}
+		}
+	}
+	for _, ip := range ips {
+		match, _ := regexp.MatchString("^192.*", ip)
+		if match {
+			Localhost = ip
+		}
+	}
+
+	env := os.Getenv("THIS_HOST")
+	if "" != env {
+		Localhost = env
+	}
+
+	fmt.Println("Localhost   : ", Localhost)
 
 }
 
 func GetPartition() {
-    value, ok := AgentConf.Partition[Localhost] 
-    if ok {
-        Partition = value    
-        fmt.Println("Partition   : ", Partition)
-    }
+	value, ok := AgentConf.Partition[Localhost]
+	if ok {
+		Partition = value
+		fmt.Println("Partition   : ", Partition)
+	}
 }
-
-
