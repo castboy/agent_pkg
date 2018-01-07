80a15b7afff13d7b581f43545d82ff5c10579463
yuexiriguang <482819895@qq.com>
2018-01-07 15:45:27 +0800

    print conf elegantly

--- conf.go
+++ conf.go
@@ -28,5 +28,19 @@ func ParseConf(bytes []byte) {
 	}
 	Log.Info("Agent Conf: %v", AgentConf)
 
-	fmt.Printf("Agent Conf: %v", AgentConf)
+	fmt.Println("Agent Conf Below:")
+	fmt.Println("EngineReqPort: ", AgentConf.EngineReqPort)
+	fmt.Println("MaxCache: ", AgentConf.MaxCache)
+	fmt.Println("Partition: ", AgentConf.Partition)
+	fmt.Println("Topic: ", AgentConf.Topic)
+	fmt.Println("Offset: ", AgentConf.Offset)
+	fmt.Println("HdfsNameNode: ", AgentConf.HdfsNameNode)
+	fmt.Println("WebServerReqIp: ", AgentConf.WebServerReqIp)
+	fmt.Println("WebServerReqPort: ", AgentConf.WebServerReqPort)
+	fmt.Println("WafInstanceSrc: ", AgentConf.WafInstanceSrc)
+	fmt.Println("WafInstanceDst: ", AgentConf.WafInstanceDst)
+	fmt.Println("OfflineMsgTopic: ", AgentConf.OfflineMsgTopic)
+	fmt.Println("OfflineMsgPartion: ", AgentConf.OfflineMsgPartion)
+	fmt.Println("ClearHdfsHdlInterval: ", AgentConf.ClearHdfsHdl)
+	fmt.Println("GetOfflineMsgInterval: ", AgentConf.GetOfflineMsg)
 }
